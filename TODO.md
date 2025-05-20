1b4b198df31b1c0ce093d5cce5084fc8848c244b
me <user@home.local>
2025-05-19 19:09:44 -0600

    recompile test3

--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
+(parsing errors)
+later: regarding real List.h:
+- why List.h has `#pragma once` twice?
+- why List.h stopped reading after a certain point?
+
 (watch mode compatibility w/ VSCode + code formatter)
 Workaround: for now, I run manually, as-needed.
 - only replace file if comment parts are different
--- tests/fixtures/test3.c
+++ tests/fixtures/test3.c
@@ -2,44 +2,66 @@
 // #macro MACRO1(T1)
 //   {{~#for T1}}
 //   // {{this.op}}
-//   extern inline void v1_{{this.op}}(v1* dst, v1* a, v1* b) {
-//     dst->x = a->x {{this.oper}} b->x;
+//   inline v1 v1_{{this.op}}(v1 a, v1 b) {
+//     v1 dst;
+//     dst.x = a.x {{this.oper}} b.x;
+//     return dst;
 //   }
 //
-//   extern inline void v2_{{this.op}}(v2* dst, v2* a, v2* b) {
-//     v1_{{this.op}}((v1*)dst, (v1*)a, (v1*)b);
-//     dst->y = a->y {{this.oper}} b->y;
+//   inline v2 v2_{{this.op}}(v2 a, v2 b) {
+//     v2 dst;
+//     dst.x = a.x {{this.oper}} b.x;
+//     dst.y = a.y {{this.oper}} b.y;
+//     return dst;
 //   }
 //
-//   extern inline void v3_{{this.op}}(v3* dst, v3* a, v3* b) {
-//     v2_{{this.op}}((v2*)dst, (v2*)a, (v2*)b);
-//     dst->z = a->z {{this.oper}} b->z;
+//   inline v3 v3_{{this.op}}(v3 a, v3 b) {
+//     v3 dst;
+//     dst.x = a.x {{this.oper}} b.x;
+//     dst.y = a.y {{this.oper}} b.y;
+//     dst.z = a.z {{this.oper}} b.z;
+//     return dst;
 //   }
 //
-//   extern inline void v4_{{this.op}}(v4* dst, v4* a, v4* b) {
-//     v3_{{this.op}}((v3*)dst, (v3*)a, (v3*)b);
-//     dst->w = a->w {{this.oper}} b->w;
+//   inline v4 v4_{{this.op}}(v4 a, v4 b) {
+//     v4 dst;
+//     dst.x = a.x {{this.oper}} b.x;
+//     dst.y = a.y {{this.oper}} b.y;
+//     dst.z = a.z {{this.oper}} b.z;
+//     dst.w = a.w {{this.oper}} b.w;
+//     return dst;
 //   }
 //
 //   // {{this.op}} scalar
 //
-//   extern inline void v1_{{this.op}}S(v1* dst, v1* a, f32 s) {
-//     dst->x = a->x {{this.oper}} s;
+//   inline v1 v1_{{this.op}}S(v1 a, f32 s) {
+//     v1 dst;
+//     dst.x = a.x {{this.oper}} s;
+//     return dst;
 //   }
 //
-//   extern inline void v2_{{this.op}}S(v2* dst, v2* a, f32 s) {
-//     v1_{{this.op}}S((v1*)dst, (v1*)a, s);
-//     dst->y = a->y {{this.oper}} s;
+//   inline v2 v2_{{this.op}}S(v2 a, f32 s) {
+//     v2 dst;
+//     dst.x = a.x {{this.oper}} s;
+//     dst.y = a.y {{this.oper}} s;
+//     return dst;
 //   }
 //
-//   extern inline void v3_{{this.op}}S(v3* dst, v3* a, f32 s) {
-//     v2_{{this.op}}S((v2*)dst, (v2*)a, s);
-//     dst->z = a->z {{this.oper}} s;
+//   inline v3 v3_{{this.op}}S(v3 a, f32 s) {
+//     v3 dst;
+//     dst.x = a.x {{this.oper}} s;
+//     dst.y = a.y {{this.oper}} s;
+//     dst.z = a.z {{this.oper}} s;
+//     return dst;
 //   }
 //
-//   extern inline void v4_{{this.op}}S(v4* dst, v4* a, f32 s) {
-//     v3_{{this.op}}S((v3*)dst, (v3*)a, s);
-//     dst->w = a->w {{this.oper}} s;
+//   inline v4 v4_{{this.op}}S(v4 a, f32 s) {
+//     v4 dst;
+//     dst.x = a.x {{this.oper}} s;
+//     dst.y = a.y {{this.oper}} s;
+//     dst.z = a.z {{this.oper}} s;
+//     dst.w = a.w {{this.oper}} s;
+//     return dst;
 //   }
 //   {{~/for~}}
 //
@@ -54,166 +76,254 @@
 // #metagen
 
 // mul
-extern inline void v1_mul(v1* dst, v1* a, v1* b) {
-  dst->x = a->x * b->x;
+inline v1 v1_mul(v1 a, v1 b) {
+  v1 dst;
+  dst.x = a.x * b.x;
+  return dst;
 }
 
-extern inline void v2_mul(v2* dst, v2* a, v2* b) {
-  v1_mul((v1*)dst, (v1*)a, (v1*)b);
-  dst->y = a->y * b->y;
+inline v2 v2_mul(v2 a, v2 b) {
+  v2 dst;
+  dst.x = a.x * b.x;
+  dst.y = a.y * b.y;
+  return dst;
 }
 
-extern inline void v3_mul(v3* dst, v3* a, v3* b) {
-  v2_mul((v2*)dst, (v2*)a, (v2*)b);
-  dst->z = a->z * b->z;
+inline v3 v3_mul(v3 a, v3 b) {
+  v3 dst;
+  dst.x = a.x * b.x;
+  dst.y = a.y * b.y;
+  dst.z = a.z * b.z;
+  return dst;
 }
 
-extern inline void v4_mul(v4* dst, v4* a, v4* b) {
-  v3_mul((v3*)dst, (v3*)a, (v3*)b);
-  dst->w = a->w * b->w;
+inline v4 v4_mul(v4 a, v4 b) {
+  v4 dst;
+  dst.x = a.x * b.x;
+  dst.y = a.y * b.y;
+  dst.z = a.z * b.z;
+  dst.w = a.w * b.w;
+  return dst;
 }
 
 // mul scalar
 
-extern inline void v1_mulS(v1* dst, v1* a, f32 s) {
-  dst->x = a->x * s;
+inline v1 v1_mulS(v1 a, f32 s) {
+  v1 dst;
+  dst.x = a.x * s;
+  return dst;
 }
 
-extern inline void v2_mulS(v2* dst, v2* a, f32 s) {
-  v1_mulS((v1*)dst, (v1*)a, s);
-  dst->y = a->y * s;
+inline v2 v2_mulS(v2 a, f32 s) {
+  v2 dst;
+  dst.x = a.x * s;
+  dst.y = a.y * s;
+  return dst;
 }
 
-extern inline void v3_mulS(v3* dst, v3* a, f32 s) {
-  v2_mulS((v2*)dst, (v2*)a, s);
-  dst->z = a->z * s;
+inline v3 v3_mulS(v3 a, f32 s) {
+  v3 dst;
+  dst.x = a.x * s;
+  dst.y = a.y * s;
+  dst.z = a.z * s;
+  return dst;
 }
 
-extern inline void v4_mulS(v4* dst, v4* a, f32 s) {
-  v3_mulS((v3*)dst, (v3*)a, s);
-  dst->w = a->w * s;
+inline v4 v4_mulS(v4 a, f32 s) {
+  v4 dst;
+  dst.x = a.x * s;
+  dst.y = a.y * s;
+  dst.z = a.z * s;
+  dst.w = a.w * s;
+  return dst;
 }
 
 // div
-extern inline void v1_div(v1* dst, v1* a, v1* b) {
-  dst->x = a->x / b->x;
+inline v1 v1_div(v1 a, v1 b) {
+  v1 dst;
+  dst.x = a.x / b.x;
+  return dst;
 }
 
-extern inline void v2_div(v2* dst, v2* a, v2* b) {
-  v1_div((v1*)dst, (v1*)a, (v1*)b);
-  dst->y = a->y / b->y;
+inline v2 v2_div(v2 a, v2 b) {
+  v2 dst;
+  dst.x = a.x / b.x;
+  dst.y = a.y / b.y;
+  return dst;
 }
 
-extern inline void v3_div(v3* dst, v3* a, v3* b) {
-  v2_div((v2*)dst, (v2*)a, (v2*)b);
-  dst->z = a->z / b->z;
+inline v3 v3_div(v3 a, v3 b) {
+  v3 dst;
+  dst.x = a.x / b.x;
+  dst.y = a.y / b.y;
+  dst.z = a.z / b.z;
+  return dst;
 }
 
-extern inline void v4_div(v4* dst, v4* a, v4* b) {
-  v3_div((v3*)dst, (v3*)a, (v3*)b);
-  dst->w = a->w / b->w;
+inline v4 v4_div(v4 a, v4 b) {
+  v4 dst;
+  dst.x = a.x / b.x;
+  dst.y = a.y / b.y;
+  dst.z = a.z / b.z;
+  dst.w = a.w / b.w;
+  return dst;
 }
 
 // div scalar
 
-extern inline void v1_divS(v1* dst, v1* a, f32 s) {
-  dst->x = a->x / s;
+inline v1 v1_divS(v1 a, f32 s) {
+  v1 dst;
+  dst.x = a.x / s;
+  return dst;
 }
 
-extern inline void v2_divS(v2* dst, v2* a, f32 s) {
-  v1_divS((v1*)dst, (v1*)a, s);
-  dst->y = a->y / s;
+inline v2 v2_divS(v2 a, f32 s) {
+  v2 dst;
+  dst.x = a.x / s;
+  dst.y = a.y / s;
+  return dst;
 }
 
-extern inline void v3_divS(v3* dst, v3* a, f32 s) {
-  v2_divS((v2*)dst, (v2*)a, s);
-  dst->z = a->z / s;
+inline v3 v3_divS(v3 a, f32 s) {
+  v3 dst;
+  dst.x = a.x / s;
+  dst.y = a.y / s;
+  dst.z = a.z / s;
+  return dst;
 }
 
-extern inline void v4_divS(v4* dst, v4* a, f32 s) {
-  v3_divS((v3*)dst, (v3*)a, s);
-  dst->w = a->w / s;
+inline v4 v4_divS(v4 a, f32 s) {
+  v4 dst;
+  dst.x = a.x / s;
+  dst.y = a.y / s;
+  dst.z = a.z / s;
+  dst.w = a.w / s;
+  return dst;
 }
 
 // add
-extern inline void v1_add(v1* dst, v1* a, v1* b) {
-  dst->x = a->x + b->x;
+inline v1 v1_add(v1 a, v1 b) {
+  v1 dst;
+  dst.x = a.x + b.x;
+  return dst;
 }
 
-extern inline void v2_add(v2* dst, v2* a, v2* b) {
-  v1_add((v1*)dst, (v1*)a, (v1*)b);
-  dst->y = a->y + b->y;
+inline v2 v2_add(v2 a, v2 b) {
+  v2 dst;
+  dst.x = a.x + b.x;
+  dst.y = a.y + b.y;
+  return dst;
 }
 
-extern inline void v3_add(v3* dst, v3* a, v3* b) {
-  v2_add((v2*)dst, (v2*)a, (v2*)b);
-  dst->z = a->z + b->z;
+inline v3 v3_add(v3 a, v3 b) {
+  v3 dst;
+  dst.x = a.x + b.x;
+  dst.y = a.y + b.y;
+  dst.z = a.z + b.z;
+  return dst;
 }
 
-extern inline void v4_add(v4* dst, v4* a, v4* b) {
-  v3_add((v3*)dst, (v3*)a, (v3*)b);
-  dst->w = a->w + b->w;
+inline v4 v4_add(v4 a, v4 b) {
+  v4 dst;
+  dst.x = a.x + b.x;
+  dst.y = a.y + b.y;
+  dst.z = a.z + b.z;
+  dst.w = a.w + b.w;
+  return dst;
 }
 
 // add scalar
 
-extern inline void v1_addS(v1* dst, v1* a, f32 s) {
-  dst->x = a->x + s;
+inline v1 v1_addS(v1 a, f32 s) {
+  v1 dst;
+  dst.x = a.x + s;
+  return dst;
 }
 
-extern inline void v2_addS(v2* dst, v2* a, f32 s) {
-  v1_addS((v1*)dst, (v1*)a, s);
-  dst->y = a->y + s;
+inline v2 v2_addS(v2 a, f32 s) {
+  v2 dst;
+  dst.x = a.x + s;
+  dst.y = a.y + s;
+  return dst;
 }
 
-extern inline void v3_addS(v3* dst, v3* a, f32 s) {
-  v2_addS((v2*)dst, (v2*)a, s);
-  dst->z = a->z + s;
+inline v3 v3_addS(v3 a, f32 s) {
+  v3 dst;
+  dst.x = a.x + s;
+  dst.y = a.y + s;
+  dst.z = a.z + s;
+  return dst;
 }
 
-extern inline void v4_addS(v4* dst, v4* a, f32 s) {
-  v3_addS((v3*)dst, (v3*)a, s);
-  dst->w = a->w + s;
+inline v4 v4_addS(v4 a, f32 s) {
+  v4 dst;
+  dst.x = a.x + s;
+  dst.y = a.y + s;
+  dst.z = a.z + s;
+  dst.w = a.w + s;
+  return dst;
 }
 
 // sub
-extern inline void v1_sub(v1* dst, v1* a, v1* b) {
-  dst->x = a->x - b->x;
+inline v1 v1_sub(v1 a, v1 b) {
+  v1 dst;
+  dst.x = a.x - b.x;
+  return dst;
 }
 
-extern inline void v2_sub(v2* dst, v2* a, v2* b) {
-  v1_sub((v1*)dst, (v1*)a, (v1*)b);
-  dst->y = a->y - b->y;
+inline v2 v2_sub(v2 a, v2 b) {
+  v2 dst;
+  dst.x = a.x - b.x;
+  dst.y = a.y - b.y;
+  return dst;
 }
 
-extern inline void v3_sub(v3* dst, v3* a, v3* b) {
-  v2_sub((v2*)dst, (v2*)a, (v2*)b);
-  dst->z = a->z - b->z;
+inline v3 v3_sub(v3 a, v3 b) {
+  v3 dst;
+  dst.x = a.x - b.x;
+  dst.y = a.y - b.y;
+  dst.z = a.z - b.z;
+  return dst;
 }
 
-extern inline void v4_sub(v4* dst, v4* a, v4* b) {
-  v3_sub((v3*)dst, (v3*)a, (v3*)b);
-  dst->w = a->w - b->w;
+inline v4 v4_sub(v4 a, v4 b) {
+  v4 dst;
+  dst.x = a.x - b.x;
+  dst.y = a.y - b.y;
+  dst.z = a.z - b.z;
+  dst.w = a.w - b.w;
+  return dst;
 }
 
 // sub scalar
 
-extern inline void v1_subS(v1* dst, v1* a, f32 s) {
-  dst->x = a->x - s;
+inline v1 v1_subS(v1 a, f32 s) {
+  v1 dst;
+  dst.x = a.x - s;
+  return dst;
 }
 
-extern inline void v2_subS(v2* dst, v2* a, f32 s) {
-  v1_subS((v1*)dst, (v1*)a, s);
-  dst->y = a->y - s;
+inline v2 v2_subS(v2 a, f32 s) {
+  v2 dst;
+  dst.x = a.x - s;
+  dst.y = a.y - s;
+  return dst;
 }
 
-extern inline void v3_subS(v3* dst, v3* a, f32 s) {
-  v2_subS((v2*)dst, (v2*)a, s);
-  dst->z = a->z - s;
+inline v3 v3_subS(v3 a, f32 s) {
+  v3 dst;
+  dst.x = a.x - s;
+  dst.y = a.y - s;
+  dst.z = a.z - s;
+  return dst;
 }
 
-extern inline void v4_subS(v4* dst, v4* a, f32 s) {
-  v3_subS((v3*)dst, (v3*)a, s);
-  dst->w = a->w - s;
+inline v4 v4_subS(v4 a, f32 s) {
+  v4 dst;
+  dst.x = a.x - s;
+  dst.y = a.y - s;
+  dst.z = a.z - s;
+  dst.w = a.w - s;
+  return dst;
 }
 // #metaend
